ae0728fee8872cbeadf34a746e97681efb711868
wanglufei <2686221966@qq.com>
2022-05-26 23:36:59 +0800

    LeetCode 2099 找到和最大的长度k的子序列

---
+++ src/com/uin/dp/maxSubsequence/Main.java
@@ -0,0 +1,28 @@
+package com.uin.dp.maxSubsequence;
+
+/**
+ * 找到和最大的长度k的子序列
+ *
+ * 给你一个整数数组 nums 和一个整数 k 。你需要找到 nums 中长度为 k 的 子序列 ，且这个子序列的 和最大 。
+ * 请你返回 任意 一个长度为 k 的整数子序列。
+ * 子序列 定义为从一个数组里删除一些元素后，不改变剩下元素的顺序得到的数组。
+ *
+ * @author wanglufei
+ * @date 2022/5/26 1:28 PM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //[2,1,3,3]
+        //2
+        int[] nums = {2,1,3,3};
+        int k =2;
+
+    }
+
+    public static int[] solution(int[] nums,int k){
+
+
+        return null;
+    }
+}
