915c8e06ab24530c689fdd0d69fc309000da452b
wanglufei <2686221966@qq.com>
2022-08-05 11:29:46 +0800

    LeetCode 114 二叉树展开为链表

---
+++ src/com/uin/leetcode_100/tree/leetcode_114/Main.java
@@ -0,0 +1,33 @@
+package com.uin.leetcode_100.tree.leetcode_114;
+
+import com.uin.leetcode_100.tree.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+/**
+ * @author wanglufei
+ */
+public class Main {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+            if (!stack.isEmpty()) {
+                curr.right = stack.peek();
+            }
+            curr.left=null;
+        }
+    }
+}
