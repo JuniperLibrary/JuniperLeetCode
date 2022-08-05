5d10b201be41fd27c6e019a259e27589fcacbf9d
wanglufei <2686221966@qq.com>
2022-08-05 14:55:50 +0800

    LeetCode 236 二叉树的最近公共祖先

---
+++ src/com/uin/leetcode_100/tree/leetcode_236/Main.java
@@ -0,0 +1,19 @@
+package com.uin.leetcode_100.tree.leetcode_236;
+
+import com.uin.leetcode_100.tree.TreeNode;
+
+/**
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * @author wanglufei
+ */
+public class Main {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        return right;
+    }
+}
