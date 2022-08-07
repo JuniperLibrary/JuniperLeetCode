b1eaead4ede9f73be7fcae1ecc80390e207e1bb3
wanglufei <2686221966@qq.com>
2022-08-07 22:04:05 +0800

    大疆秋招 获取最大的收益

--- src/com/uin/dj/sc1/Main.java
+++ src/com/uin/dj/sc1/Main.java
@@ -18,24 +18,15 @@ package com.uin.dj.sc1;
  * 抓取到的最高金额 = 4 + 2 = 6 。
  * <p>
  * 示例 2：
- * <p>
  * 输入：[2,7,1,3,5]
- * <p>
  * 输出：12
- * <p>
  * 解释：抓取 2 号物品 (金额 = 7), 抓取 5 号物品 (金额 = 5)。
- * <p>
  * 抓取到的最高金额 = 7 + 5 = 12 。
- * <p>
- * <p>
- * <p>
  * 输入描述
  * 输入为路线上的物品价值, 输入为一个数组，使用空格隔开
  * <p>
  * 输出描述
  * 最终能获取的最大价值
- * <p>
- * <p>
  * 样例输入
  * 2 7 1 3 5
  * 样例输出
@@ -54,7 +45,7 @@ public class Main {
         String s = in.nextLine();
         String[] split = s.split("\\s+");
         int[] nums = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
-        System.out.println(helper(nums));
+        System.out.println(solution1(nums));
     }
 
     public static int helper(int[] nums) {
@@ -62,21 +53,59 @@ public class Main {
         int max = 0;
 
         //第一次抓的
-        int max1 = 0;
         int index = 0;
+        int max1 = Arrays.stream(nums).max().getAsInt();
         for (int i = 0; i < nums.length; i++) {
-            max1 = Math.max(max1, nums[i]);
-            index = i;
+            if (max1 == nums[i]) index = i;
         }
 
         //冷却时间恰好飞过两个物品（即飞机冷却时间内无法抓取物品）
-        if (index <= nums.length && index + 1 <= nums.length) return max1;
+        if (index > nums.length && index + 1 > nums.length) return max1;
 
         int max2 = 0;
-        for (int i = index; i <= nums.length; i += 2) {
-            max2 = Math.max(max2, nums[i]);
+        index += 1;
+        while (index < nums.length) {
+            for (int i = index; i < nums.length; i++) {
+                max2 = Math.max(max2, nums[i]);
+            }
         }
         max = max1 + max2;
         return max;
+    }
+
+    public static int solution1(int[] nums) {
+        int n = nums.length;
+        if (nums == null || n == 0) return 0;
+
+        //状态容器
+        int[] R = new int[n];
+        int[] NR = new int[n];
+
+        //初始化
+        R[0] = nums[0];
+        NR[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            R[i] = nums[i] + NR[i - 1];
+            NR[i] = Math.max(NR[i - 1], R[i - 1]);
+        }
+        return Math.max(NR[n - 1], R[n - 1]);
+    }
+
+    public static int solution2(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+
+        // 状态容器初始化
+        int R = 0;
+        int NR = 0;
+
+        //状态转移方程
+        for (int i = 0; i < nums.length; i++) {
+            int max = Math.max(R, NR);
+            R = nums[i] + NR;
+            NR = max;
+        }
+        return Math.max(R, NR);
     }
 }
