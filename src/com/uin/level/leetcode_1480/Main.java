b9e1b97d01472649572820457576d24df0513e9b
wanglufei <2686221966@qq.com>
2022-08-12 15:35:16 +0800

    LeetCode 1480 一维数组的动态和

--- src/com/uin/level/leetcode_1480/Main.java
+++ src/com/uin/level/leetcode_1480/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     public static int[] runningSum(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
+            nums[i] = nums[i] + nums[i - 1];
         }
         return nums;
     }
