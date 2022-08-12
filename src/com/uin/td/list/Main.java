908deee71903384eab0759012ef874c6d5b6514d
wanglufei <2686221966@qq.com>
2022-08-12 11:21:29 +0800

    出现次数大于等于数组长度一半的数

---
+++ src/com/uin/td/list/Main.java
@@ -0,0 +1,76 @@
+package com.uin.td.list;
+
+import java.util.*;
+
+/**
+ * 出现次数大于等于数组长度一半的数
+ *
+ * @author wanglufei
+ * @date 2022/8/11 7:34 PM
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = in.nextInt();
+        }
+        System.out.println(helper(nums));
+    }
+
+    public static int helper(int[] nums) {
+        int length = nums.length;
+        if (length == 0) return 0;
+
+        Arrays.sort(nums);
+
+        int count = 0;
+        int last = length / 2;
+        int index = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                count++;
+                if (count >= last) {
+                    index = i;
+                }
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return nums[index];
+    }
+
+    public static int helper2(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    public static int helper3(int[] nums) {
+        Map<Integer, Integer> map = map(nums);
+        Map.Entry<Integer, Integer> mapEntry = null;
+        for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
+            if (mapEntry == null || integerIntegerEntry.getValue() > mapEntry.getValue()) {
+                mapEntry = integerIntegerEntry;
+            }
+        }
+        return mapEntry.getKey();
+    }
+
+    public static Map<Integer, Integer> map(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+        return map;
+    }
+
+}
