f0977b42d4a097681ada156c0c7047f3e1347f05
wanglufei <2686221966@qq.com>
2022-08-15 01:15:40 +0800

    LeetCode 19. 删除链表的倒数第 N 个结点 递归

--- src/com/uin/level/leetcode_19/Main.java
+++ src/com/uin/level/leetcode_19/Main.java
@@ -7,6 +7,15 @@ import com.uin.leetcode_100.linkedlist.ListNode;
  * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
  */
 public class Main {
+    /**
+     * 快慢指针
+     *
+     * @param head
+     * @param n
+     * @return com.uin.leetcode_100.linkedlist.ListNode
+     * @author wanglufei
+     * @date 2022/8/15 1:03 AM
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode fast = head, slow = head;
         //  快指针先走n步
@@ -14,18 +23,37 @@ public class Main {
             fast = fast.next;
         }
         /**
-        while (n>0){
-            fast=fast.next;
-            n--;
-        }*/
+         while (n>0){
+         fast=fast.next;
+         n--;
+         }*/
         //conner case
-        if (fast==null) return head.next;
+        if (fast == null) return head.next;
         // 再同时走一步
         while (fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }
         slow.next = slow.next.next;
+        return head;
+    }
+
+    /**
+     * 递归
+     *
+     * @param head
+     * @param n
+     * @return com.uin.leetcode_100.linkedlist.ListNode
+     * @author wanglufei
+     * @date 2022/8/15 1:03 AM
+     */
+    int count = 0;
+
+    public ListNode recursive(ListNode head, int n) {
+        if (head == null) return null;
+        head.next = recursive(head.next, n);
+        count++;
+        if (count == n) return head.next;
         return head;
     }
 }
