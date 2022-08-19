cc067de857071e94bfbce2cf51cf0a158f402f14
wanglufei <2686221966@qq.com>
2022-08-19 16:05:44 +0800

    LeetCode 98. 验证二叉搜索树、235. 二叉搜索树的最近公共祖先、236. 二叉树的最近公共祖先

--- src/com/uin/leetcode_100/tree/leetcode_236/Main.java
+++ src/com/uin/leetcode_100/tree/leetcode_236/Main.java
@@ -10,11 +10,16 @@ import com.uin.leetcode_100.tree.TreeNode;
  */
 public class Main {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //conner case
         if (root == null || root == p || root == q) return root;
+
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
+
         if (left != null && right != null) return root;
+
         if (left != null) return left;
+
         return right;
     }
 }
