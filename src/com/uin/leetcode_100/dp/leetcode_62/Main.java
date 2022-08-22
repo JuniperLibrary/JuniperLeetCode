8fcdb1269336494767d49d24110d6e0ae344efc8
wanglufei <2686221966@qq.com>
2022-08-22 14:25:29 +0800

    LeetCode 62. 不同路径 dp

--- src/com/uin/leetcode_100/dp/leetcode_62/Main.java
+++ src/com/uin/leetcode_100/dp/leetcode_62/Main.java
@@ -1,5 +1,13 @@
 package com.uin.leetcode_100.dp.leetcode_62;
 
+/**
+ * 62. 不同路径
+ * 一个机器人位于一个 m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * <p>
+ * 问总共有多少条不同的路径？
+ */
 public class Main {
     public int uniquePaths(int m, int n) {
         int[] dp = new int[m];
