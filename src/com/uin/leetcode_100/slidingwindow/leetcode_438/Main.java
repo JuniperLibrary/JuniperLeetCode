019e4fe3fcd1f4c31927360e256aff15f0deefbb
wanglufei <2686221966@qq.com>
2022-08-23 11:14:38 +0800

    LeetCode 438. 找到字符串中所有字母异位词 滑动窗口

--- src/com/uin/leetcode_100/slidingwindow/leetcode_438/Main.java
+++ src/com/uin/leetcode_100/slidingwindow/leetcode_438/Main.java
@@ -4,35 +4,46 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 438. 找到字符串中所有字母异位词
+ * 给定两个字符串s和 p，找到s中所有p的异位词的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+ * <p>
+ * 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+ * 输入: s = "cbaebabacd", p = "abc"
+ * 输出: [0,6]
+ * 解释:
+ * 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+ * 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+ */
 public class Main {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
+        int sLen = s.length(), pLen = p.length();
 
-        int plength = s.length();
-        int slength = p.length();
-
-        if (plength > slength) return res;
-
-        int[] arrs = new int[26];
-        int[] arrp = new int[26];
-
-        for (int i = 0; i < plength; i++) {
-            arrs[s.charAt(i) - 'a'] += 1;
-            arrp[s.charAt(i) - 'a'] += 1;
+        if (sLen < pLen) {
+            return new ArrayList<Integer>();
         }
 
-        if (Arrays.equals(arrs, arrp)) {
+        List<Integer> res = new ArrayList<Integer>();
+        int[] sCount = new int[26];
+        int[] pCount = new int[26];
+        for (int i = 0; i < pLen; ++i) {
+            ++sCount[s.charAt(i) - 'a'];
+            ++pCount[p.charAt(i) - 'a'];
+        }
+
+        if (Arrays.equals(sCount, pCount)) {
             res.add(0);
         }
 
-        for (int i = 1; plength + i - 1 < slength; i++) {
-            arrs[s.charAt(i - 1) - 'a'] -= 1;
-            arrs[s.charAt(plength + i - 1) - 'a'] += 1;
+        for (int i = 0; i < sLen - pLen; ++i) {
+            --sCount[s.charAt(i) - 'a'];
+            ++sCount[s.charAt(i + pLen) - 'a'];
 
-            if (Arrays.equals(arrs, arrp)) {
-                res.add(i);
+            if (Arrays.equals(sCount, pCount)) {
+                res.add(i + 1);
             }
         }
+
         return res;
     }
 
