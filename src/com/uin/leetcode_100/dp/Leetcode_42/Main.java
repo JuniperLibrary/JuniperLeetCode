ada6c368e6a3ed320f34bf4b7fe78fb1be1f933a
wanglufei <2686221966@qq.com>
2022-08-30 11:22:53 +0800

    LeetCode 42. 接雨水 dp

--- src/com/uin/leetcode_100/dp/Leetcode_42/Main.java
+++ src/com/uin/leetcode_100/dp/Leetcode_42/Main.java
@@ -1,7 +1,14 @@
-package com.uin.leetcode_100.array.Leetcode_42;
+package com.uin.leetcode_100.dp.Leetcode_42;
 
 import java.util.Stack;
 
+/**
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ */
 public class Main {
     public static void main(String[] args) {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
