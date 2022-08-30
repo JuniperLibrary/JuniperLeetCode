fa90247ac1eb05036c76fa64a5fc970613aa3e27
wanglufei <2686221966@qq.com>
2022-08-30 11:22:37 +0800

    LeetCode 621. 任务调度器

---
+++ src/com/uin/level2/leetcode_621/Main.java
@@ -0,0 +1,43 @@
+package com.uin.level2.leetcode_621;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 621. 任务调度器
+ * 给你一个用字符数组tasks 表示的 CPU 需要执行的任务列表。其中每个字母表示一种不同种类的任务。任务可以以任意顺序执行，
+ * 并且每个任务都可以在 1 个单位时间内执行完。
+ * 在任何一个单位时间，CPU 可以完成一个任务，或者处于待命状态。
+ * 然而，两个 相同种类 的任务之间必须有长度为整数 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+ * 你需要计算完成所有任务所需要的 最短时间 。
+ * <p>
+ * 输入：tasks = ["A","A","A","B","B","B"], n = 2
+ * 输出：8
+ * 解释：A -> B -> (待命) -> A -> B -> (待命) -> A -> B
+ * 在本示例中，两个相同类型任务之间必须间隔长度为 n = 2 的冷却时间，而执行一个任务只需要一个单位时间，所以中间出现了（待命）状态。
+ */
+public class Main {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> freq = new HashMap<Character, Integer>();
+        // 最多的执行次数
+        int maxExec = 0;
+        for (char ch : tasks) {
+            int exec = freq.getOrDefault(ch, 0) + 1;
+            freq.put(ch, exec);
+            maxExec = Math.max(maxExec, exec);
+        }
+
+        // 具有最多执行次数的任务数量
+        int maxCount = 0;
+        Set<Map.Entry<Character, Integer>> entrySet = freq.entrySet();
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            int value = entry.getValue();
+            if (value == maxExec) {
+                ++maxCount;
+            }
+        }
+
+        return Math.max((maxExec - 1) * (n + 1) + maxCount, tasks.length);
+    }
+}
