5a598bbd323fce0e6cd4920fca11b924c29e42ef
wanglufei <2686221966@qq.com>
2022-08-31 09:17:55 +0800

    LeetCode 226. 翻转二叉树

---
+++ src/com/uin/level2/leetcode_226/Main.java
@@ -0,0 +1,48 @@
+package com.uin.level2.leetcode_226;
+
+import com.uin.leetcode_100.tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 226. 翻转二叉树
+ */
+public class Main {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        invertTree(root.left);
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        return root;
+    }
+
+    public TreeNode helper(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+            TreeNode node  = queue.poll();
+            TreeNode rightTree = node.right;
+            node.right=node.left;
+            node.left=rightTree;
+
+            if (node.left!=null){
+                queue.offer(node.left);
+            }
+
+            if (node.right!=null){
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+}
