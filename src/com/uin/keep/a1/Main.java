2ee3f1713cd0577c7a6c935addbecd1dff265a60
wanglufei <2686221966@qq.com>
2022-08-31 09:18:12 +0800

    2023届Keep秋招后端卷1

---
+++ src/com/uin/keep/a1/Main.java
@@ -0,0 +1,18 @@
+package com.uin.keep.a1;
+
+/**
+ * 2023届Keep秋招后端卷1-劳逸结合
+ * 如果小k睡觉的同时闹钟有想起的话，休息时间就为0；
+ * 返回能够小k能够休息多久
+ * 输入案例：
+ * - [6,13]  [[8,0]]
+ * 输出案例：
+ * - [1,47]
+ * 解释：
+ * - x ：0 当作x : 60。60-13=47
+ */
+public class Main {
+    public int[] helper(int[] timeTable, int[][] alarms) {
+        return new int[0];
+    }
+}
---
+++ src/com/uin/keep/a2/Main.java
@@ -0,0 +1,20 @@
+package com.uin.keep.a2;
+
+/**
+ * 2023届Keep秋招后端卷1-小明改分
+ * 小k5科成绩总分123，每个数字只能改0次或1次，使得成绩最高
+ * 输入案例：
+ * - 123
+ * 输出案例
+ * - 321
+ * <p>
+ * 输入案例：
+ * - 421
+ * 输出案例：
+ * - 421
+ */
+public class Main {
+    public int helper(int num) {
+        return num;
+    }
+}
