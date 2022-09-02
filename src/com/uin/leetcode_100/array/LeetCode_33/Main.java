622c8a33f1a1d46c2e4a9680b9e1c456d8f9cccc
wanglufei <2686221966@qq.com>
2022-09-02 08:36:18 +0800

    LeetCode 33 搜索旋转排序数组 二分

--- src/com/uin/leetcode_100/array/LeetCode_33/Main.java
+++ src/com/uin/leetcode_100/array/LeetCode_33/Main.java
@@ -2,9 +2,12 @@ package com.uin.leetcode_100.array.LeetCode_33;
 
 /**
  * LeetCode 33 搜索旋转排序数组
- *
- * @author wanglufei
- * @date 2022/7/11 1:43 PM
+ * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+ * 使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+ * 例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为[4,5,6,7,0,1,2] 。
+ * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
+ * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
  */
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +20,7 @@ public class Main {
     public static int solution(int nums[], int target) {
         int left = 0;
         int right = nums.length - 1;
-        int mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
         while (left <= right) {
             if (nums[mid] == target) {
                 return mid;
