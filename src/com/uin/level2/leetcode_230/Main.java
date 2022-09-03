8100c0e6ddf20ede29ac082b911acf9e299bf488
wanglufei <2686221966@qq.com>
2022-09-03 14:52:45 +0800

    LeetCode 230. 二叉搜索树中第K小的元素  因为二叉搜索树和中序遍历的性质，所以二叉搜索树的中序遍历是按照键增加的顺序进行的。于是，我们可以通过中序遍历找到第 k 个最小元素。

---
+++ src/com/uin/level2/leetcode_230/Main.java
@@ -0,0 +1,30 @@
+package com.uin.level2.leetcode_230;
+
+import com.uin.leetcode_100.tree.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 230. 二叉搜索树中第K小的元素
+ * 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+ */
+public class Main {
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            --k;
+            if (k == 0) {
+                break;
+            }
+            root = root.right;
+        }
+        return root.val;
+    }
+}
