91bce3648d4dd79111c0c20f9d13c7e268494d13
wanglufei <2686221966@qq.com>
2022-09-04 09:44:54 +0800

    leetcode 51.N 皇后 dfs

--- src/com/uin/backtracking/leetcode_51/Main.java
+++ src/com/uin/backtracking/leetcode_51/Main.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
+/**
+ * leetcode 51.N 皇后
+ * 按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+ * n皇后问题 研究的是如何将 n个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给你一个整数 n ，返回所有不同的n皇后问题 的解决方案。
+ * 每一种解法包含一个不同的n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ */
 public class Main {
     List<List<String>> res = new ArrayList<>();
     int n;
