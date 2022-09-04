b7ce72e57ff1e5089f5ed80ab3a131fee816911e
wanglufei <2686221966@qq.com>
2022-09-04 14:47:18 +0800

    LeetCode 75 颜色分类 双指针

--- src/com/uin/leetcode_100/array/LeetCode_75/Main.java
+++ src/com/uin/leetcode_100/array/LeetCode_75/Main.java
@@ -1,5 +1,11 @@
 package com.uin.leetcode_100.array.LeetCode_75;
 
+/**
+ * LeetCode 75 颜色分类
+ * 给定一个包含红色、白色和蓝色、共n 个元素的数组nums，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ * 我们使用整数 0、1 和 2 分别表示红色、白色和蓝色。
+ * 必须在不使用库的sort函数的情况下解决这个问题。
+ */
 public class Main {
     public void colorSort(int[] nums) {
         //双指针
