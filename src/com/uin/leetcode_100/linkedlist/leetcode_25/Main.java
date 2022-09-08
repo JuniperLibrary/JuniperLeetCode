d1680a2e5469f1a794ce51ad293e616d3a10c22a
wanglufei <2686221966@qq.com>
2022-09-08 09:19:26 +0800

    LeetCode 25. K 个一组翻转链表

---
+++ src/com/uin/leetcode_100/linkedlist/leetcode_25/Main.java
@@ -0,0 +1,54 @@
+package com.uin.leetcode_100.linkedlist.leetcode_25;
+
+import com.uin.leetcode_100.linkedlist.ListNode;
+
+import java.util.List;
+
+
+/**
+ * 25. K 个一组翻转链表
+ * 给你链表的头节点 head ，每...k...个节点一组进行翻转，请你返回修改后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是...k...的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ */
+public class Main {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+}
