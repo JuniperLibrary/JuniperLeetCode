daec1ebe22f35cbeba3596f56bf1bb2889d2be0b
wanglufei <2686221966@qq.com>
2022-09-08 15:51:48 +0800

    腾讯音乐娱乐集团2022暑期实习生招聘技术类岗位编程题合集--删层子树

--- src/com/uin/tencent/tree/Main.java
+++ src/com/uin/tencent/tree/Main.java
@@ -1,16 +1,42 @@
 package com.uin.tencent.tree;
 
-import javax.swing.tree.TreeNode;
-import java.util.ArrayList;
+import com.uin.leetcode_100.tree.TreeNode;
+
+import java.util.*;
 
 /**
- * https://www.nowcoder.com/test/question/done?tid=58612774&qid=2589594#summary
- *
- * @author wanglufei
- * @date 2022/7/23 2:52 PM
+ * 删层子树
+ * 牛牛有一棵n个节点的二叉树，该二叉树每个节点的权值为1。牛牛想要删掉该树其中的k层节点，删除序列为。
  */
 public class Main {
-    public void solution(TreeNode root, ArrayList<Integer> a) {
+    public ArrayList<TreeNode> deleteLevel(TreeNode root, ArrayList<Integer> a) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : a) {
+            set.add(num);
+        }
+        set.add(0);
+        Queue<TreeNode> queue = new LinkedList<>();
+        ArrayList<TreeNode> list = new ArrayList<>();
+        queue.add(root);
+        int depth = 0;
 
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+                if (set.contains(depth - 1) && !set.contains(depth))
+                    list.add(node);
+                if (!set.contains(depth) && set.contains(depth + 1)) {
+                    node.left = null;
+                    node.right = null;
+                }
+            }
+        }
+        return list;
     }
 }
