6c39ada85070f030e65f97a4ae118852c82e7c3a
wanglufei <2686221966@qq.com>
2022-09-08 13:34:08 +0800

    LeetCode 264. 丑数 II 最小堆+dp

---
+++ src/com/uin/leetcode_100/dp/leetcode_264/Main.java
@@ -0,0 +1,85 @@
+package com.uin.leetcode_100.dp.leetcode_264;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * 264. 丑数 II
+ * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ * <p>
+ * 输入：n = 10
+ * 输出：12
+ * 解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+ */
+public class Main {
+    /**
+     * 最小堆
+     * 要得到从小到大的第 nn 个丑数，可以使用最小堆实现。
+     * <p>
+     * 初始时堆为空。首先将最小的丑数 11 加入堆。
+     * <p>
+     * 每次取出堆顶元素 xx，则 xx 是堆中最小的丑数，由于 2x, 3x, 5x2x,3x,5x 也是丑数，因此将 2x, 3x, 5x2x,3x,5x 加入堆。
+     * <p>
+     * 上述做法会导致堆中出现重复元素的情况。为了避免重复元素，可以使用哈希集合去重，避免相同元素多次加入堆。
+     * <p>
+     * 在排除重复元素的情况下，第 nn 次从最小堆中取出的元素即为第 nn 个丑数。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode.cn/problems/ugly-number-ii/solution/chou-shu-ii-by-leetcode-solution-uoqd/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @return int
+     * @author wanglufei
+     * @date 2022/9/8 10:24 AM
+     */
+    public int helper(int n) {
+        int[] factors = {2, 3, 5};
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+
+    /**
+     * dp
+     *
+     * @param n
+     * @return int
+     * @author wanglufei
+     * @date 2022/9/8 10:44 AM
+     */
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(Math.min(num2, num3), num5);
+            if (dp[i] == num2) {
+                p2++;
+            }
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+        }
+        return dp[n];
+    }
+}
