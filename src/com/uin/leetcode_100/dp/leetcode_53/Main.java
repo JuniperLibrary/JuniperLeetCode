4a921b416e4731c3a9156162c2e2d6777dcba82c
wanglufei <2686221966@qq.com>
2022-09-08 10:18:13 +0800

    LeetCode 53. 最大子数组和 dp

---
+++ src/com/uin/leetcode_100/dp/leetcode_53/Main.java
@@ -0,0 +1,37 @@
+package com.uin.leetcode_100.dp.leetcode_53;
+
+/**
+ * 53. 最大子数组和
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 子数组 是数组中的一个连续部分。
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ */
+public class Main {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (dp[i - 1] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+
+    public int helper(int[] nums) {
+        int pre = 0;
+        int res = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            res = Math.max(res, pre);
+        }
+        return res;
+    }
+}
