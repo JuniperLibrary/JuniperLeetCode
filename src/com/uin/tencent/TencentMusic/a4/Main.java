8ec26d0aa09328e952bd82a2c40f5d79e963a6f1
wanglufei <2686221966@qq.com>
2022-09-08 15:49:57 +0800

    腾讯音乐娱乐集团2022暑期实习生招聘技术类岗位编程题合集--出现至少k次的小写字母 map

---
+++ src/com/uin/tencent/TencentMusic/a4/Main.java
@@ -0,0 +1,28 @@
+package com.uin.tencent.TencentMusic.a4;
+
+import java.util.HashMap;
+
+/**
+ * 腾讯音乐娱乐集团2022暑期实习生招聘技术类岗位编程题合集--出现至少k次的小写字母
+ * 给定一个由小写字母组成的字符串 s，求有多少小写字母出现了至少 k 次。
+ * "aaabcd",2
+ * 1
+ */
+public class Main {
+    public int howMany(String S, int k) {
+        // write code here
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int ans = 0;
+        for (Character ch : map.keySet()) {
+            if (map.get(ch) >= k) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
