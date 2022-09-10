1bfbbee75102b526e96fbc32b32e8bcb9321b3a6
wanglufei <2686221966@qq.com>
2022-09-10 09:36:15 +0800

    11. 盛最多水的容器  前缀和 接雨水问题 https://labuladong.github.io/algo/4/33/129/

---
+++ src/com/uin/leetcode_100/partialsum/leetcode_11/Main.java
@@ -0,0 +1,31 @@
+package com.uin.leetcode_100.partialsum.leetcode_11;
+
+/**
+ * 11. 盛最多水的容器
+ * 给定一个长度为 n 的整数数组height。有n条垂线，第 i 条线的两个端点是(i, 0)和(i, height[i])。
+ * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ * 说明：你不能倾斜容器。
+ * <p>
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为49。
+ */
+public class Main {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int res = 0;
+        while (left < right) {
+            // [left, right] 之间的矩形面积
+            int cur_area = Math.min(height[left], height[right]) * (right - left);
+            res = Math.max(res, cur_area);
+            // 双指针技巧，移动较低的一边
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+}
