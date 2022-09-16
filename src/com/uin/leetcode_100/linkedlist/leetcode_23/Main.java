a707096cd2f04f46b07f5516301a12965d60ff72
wanglufei <2686221966@qq.com>
2022-09-16 14:45:53 +0800

    leetcode 23.合并K个升序链表

---
+++ src/com/uin/leetcode_100/linkedlist/leetcode_23/Main.java
@@ -0,0 +1,48 @@
+package com.uin.leetcode_100.linkedlist.leetcode_23;
+
+import com.uin.leetcode_100.linkedlist.ListNode;
+
+/**
+ * 23. 合并K个升序链表
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ */
+public class Main {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+        if (lists.length == 1)
+            return lists[0];
+        if (lists.length == 2) {
+            return mergeTwoLists(lists[0], lists[1]);
+        }
+
+        int mid = lists.length / 2;
+        ListNode[] l1 = new ListNode[mid];
+        for (int i = 0; i < mid; i++) {
+            l1[i] = lists[i];
+        }
+
+        ListNode[] l2 = new ListNode[lists.length - mid];
+        for (int i = mid, j = 0; i < lists.length; i++, j++) {
+            l2[j] = lists[i];
+        }
+
+        return mergeTwoLists(mergeKLists(l1), mergeKLists(l2));
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head = null;
+        if (l1.val <= l2.val) {
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
