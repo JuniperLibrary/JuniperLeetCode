e1bf5555faff7ddbe4add05a92279f8d7264e279
wanglufei <2686221966@qq.com>
2022-09-16 15:28:12 +0800

    leetcode 98. 验证二叉搜索树

--- src/com/uin/leetcode_100/tree/leetcode_98/Main.java
+++ src/com/uin/leetcode_100/tree/leetcode_98/Main.java
@@ -7,11 +7,10 @@ import java.util.List;
 
 /**
  * 98. 验证二叉搜索树
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
  * 节点的左子树只包含 小于 当前节点的数。
  * 节点的右子树只包含 大于 当前节点的数。
  * 所有左子树和右子树自身必须也是二叉搜索树。
- *
- * @author wanglufei
  */
 public class Main {
     public boolean isValidBST(TreeNode root) {
@@ -35,7 +34,6 @@ public class Main {
      * @date 2022/8/19 3:23 PM
      */
     List<Integer> list = new ArrayList<>();
-
     public boolean isBSTValid(TreeNode root) {
         if (root == null) return true;
         traversal(root);//inorder
