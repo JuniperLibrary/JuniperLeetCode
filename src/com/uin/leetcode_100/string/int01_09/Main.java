c42c82ce86047d390e0e16dd0bd03b23f3c0d808
wanglufei <2686221966@qq.com>
2022-09-29 07:57:46 +0800

    面试题 01.09. 字符串轮转

---
+++ src/com/uin/leetcode_100/string/int01_09/Main.java
@@ -0,0 +1,38 @@
+package com.uin.leetcode_100.string.int01_09;
+
+/**
+ * 面试题 01.09. 字符串轮转
+ * 字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+ */
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "waterbottle";
+        String s2 = "erbottlewat";
+        System.out.println(isFlipedString(s1, s2));
+    }
+
+    public static boolean isFlipedString(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        if (m != n) {
+            return false;
+        }
+
+        if (n == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt((i + j) % n) != s2.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
