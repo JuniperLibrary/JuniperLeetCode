108ef1bca0c23fa4fb95e1b9f52477b7cfb573f4
wanglufei <2686221966@qq.com>
2022-10-31 12:41:46 +0800

    LeetCode 784. 字母大小写全排列

---
+++ src/com/uin/leetcode_100/string/leetcode_784/Main.java
@@ -0,0 +1,68 @@
+package com.uin.leetcode_100.string.leetcode_784;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 784. 字母大小写全排列
+ * 给定一个字符串 s ，通过将字符串 s 中的每个字母转变大小写，我们可以获得一个新的字符串。
+ * 返回 所有可能得到的字符串集合 。以 任意顺序 返回输出。
+ * 示例 1：
+ * 输入：s = "a1b2"
+ * 输出：["a1b2", "a1B2", "A1b2", "A1B2"]
+ * 示例 2:
+ * <p>
+ * 输入: s = "3z4"
+ * 输出: ["3z4","3Z4"]
+ * <p>
+ */
+public class Main {
+    public static void main(String[] args) {
+        String s = "a1b2";
+        System.out.println(letterCasePermutation(s));
+    }
+
+    public static List<String> letterCasePermutation(String s) {
+        List<String> ans = new ArrayList<>();
+        Queue<StringBuilder> queue = new ArrayDeque<>();
+        queue.offer(new StringBuilder());
+        while (!queue.isEmpty()) {
+            StringBuilder cur = queue.peek();
+            if (cur.length() == s.length()) {
+                ans.add(cur.toString());
+                queue.poll();
+            } else {
+                int pos = cur.length();
+                if (Character.isLetter(s.charAt(pos))) {
+                    StringBuilder next = new StringBuilder(cur);
+                    next.append((char) (s.charAt(pos) ^ 32));
+                    queue.offer(next);
+                }
+                cur.append(s.charAt(pos));
+            }
+        }
+        return ans;
+    }
+
+    public static List<String> helper(String s) {
+        List<String> ans = new ArrayList<>();
+        dfs(s.toCharArray(), 0, ans);
+        return ans;
+    }
+
+    private static void dfs(char[] arr, int pos, List<String> res) {
+        while (pos < arr.length && Character.isDigit(arr[pos])) {
+            pos++;
+        }
+        if (pos == arr.length) {
+            res.add(new String(arr));
+            return;
+        }
+        arr[pos] ^= 32;
+        dfs(arr, pos + 1, res);
+        arr[pos] ^= 32;
+        dfs(arr, pos + 1, res);
+    }
+}
