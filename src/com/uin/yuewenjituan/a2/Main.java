d2fb69642f0f2dc6a7e6a35dfbf4ec5840fc1d67
wanglufei <2686221966@qq.com>
2022-11-01 22:37:04 +0800

    LeetCode 88. 合并两个有序数组

---
+++ src/com/uin/yuewenjituan/a2/Main.java
@@ -0,0 +1,77 @@
+package com.uin.yuewenjituan.a2;
+
+import java.util.Arrays;
+
+/**
+ * 88. 合并两个有序数组
+ * 给你两个按 非递减顺序 排列的整数数组nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * <p>
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * <p>
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，
+ * 后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ * <p>
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+ * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0}, nums2 = {2, 5, 6};
+        int m = 3, n = 3;
+        helper(nums1, m, nums2, n);
+    }
+
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * @param nums1 int整型一维数组
+     * @param m     int整型
+     * @param nums2 int整型一维数组
+     * @param n     int整型
+     * @return int整型一维数组
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // write code here
+        // [1,2,3,0,0,0] m=3
+        // [2,5,6]  n=3
+        // merge [1,2,2,3,5,6]
+        for (int i = 0; i != n; ++i) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+
+    /**
+     * 返回合并之后的结果
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     * @return
+     */
+    public static void helper(int[] nums1, int m, int[] nums2, int n) {
+        int i = nums1.length;
+        while (n > 0) {
+            if (m > 0 && nums1[m - 1] > nums2[n - 1]) {
+                nums1[i--] = nums2[--m];
+            } else {
+                nums1[--i] = nums2[n--];
+            }
+        }
+    }
+
+    public static void solution(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m - 1;
+        int len2 = n - 1;
+        int len = m + n - 1;
+        while (len1 >= 0 && len2 >= 0) {
+            // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
+            nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+        }
+        // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
+        System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
+    }
+}
