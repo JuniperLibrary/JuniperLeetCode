0182211c04dec0a969de63b46377f53859620899
wanglufei <2686221966@qq.com>
2023-01-08 20:06:11 +0800

    LeetCode 6285. 执行 K 次操作后的最大分数

---
+++ src/com/weekendAC/weenkend_327/leetcode_6285/Main.java
@@ -0,0 +1,55 @@
+package com.weekendAC.weenkend_327.leetcode_6285;
+
+import java.util.PriorityQueue;
+
+/**
+ * 6285. 执行 K 次操作后的最大分数
+ * 给你一个下标从 0 开始的整数数组 nums 和一个整数 k 。你的 起始分数 为 0 。
+ * <p>
+ * 在一步 操作 中：
+ * <p>
+ * 选出一个满足 0 <= i < nums.length 的下标 i ，
+ * 将你的 分数 增加 nums[i] ，并且
+ * 将 nums[i] 替换为 ceil(nums[i] / 3) 。
+ * 返回在 恰好 执行 k 次操作后，你可能获得的最大分数。
+ * <p>
+ * 向上取整函数 ceil(val) 的结果是大于或等于 val 的最小整数。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [10,10,10,10,10], k = 5
+ * 输出：50
+ * 解释：对数组中每个元素执行一次操作。最后分数是 10 + 10 + 10 + 10 + 10 = 50 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,10,3,3,3], k = 3
+ * 输出：17
+ * 解释：可以执行下述操作：
+ * 第 1 步操作：选中 i = 1 ，nums 变为 [1,4,3,3,3] 。分数增加 10 。
+ * 第 2 步操作：选中 i = 1 ，nums 变为 [1,2,3,3,3] 。分数增加 4 。
+ * 第 3 步操作：选中 i = 2 ，nums 变为 [1,1,1,3,3] 。分数增加 3 。
+ * 最后分数是 10 + 4 + 3 = 17 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/maximal-score-after-applying-k-operations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main {
+    public long maxKelements(int[] nums, int k) {
+        long ans = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (int num : nums) {
+            heap.offer(num);
+        }
+
+        while (!heap.isEmpty() && k > 0) {
+            Integer poll = heap.poll();
+            ans += poll;
+            heap.offer((int) (Math.ceil(((double) poll) / 3)));
+            k--;
+        }
+        return ans;
+    }
+}
