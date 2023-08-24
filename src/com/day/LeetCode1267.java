6ac0f69f3877f9dc7819a8cbdcc6a8a20ace7862
DingChuanV <2686221966@qq.com>
2023-08-24 23:23:26 +0800

    2023 8.24

---
+++ src/com/day/LeetCode1267.java
@@ -0,0 +1,31 @@
+package com.day;
+
+public class LeetCode1267 {
+
+  public int countServers(int[][] grid) {
+    int rows = grid.length;
+    int cols = grid[0].length;
+
+    int[] rowServers = new int[rows];
+    int[] colServers = new int[cols];
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (grid[i][j] == 1) {
+          rowServers[i]++;
+          colServers[j]++;
+        }
+      }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (grid[i][j] == 1 && (rowServers[i] > 1 || colServers[j] > 1)) {
+         ans++;
+        }
+      }
+    }
+    return ans;
+  }
+}
