e47dceeb25c52948bdaf80ccce8b29596d53ae10
DingChuanV <2686221966@qq.com>
2023-09-21 21:54:12 +0800

    广联达

---
+++ src/com/uin/guanlianda/Main1.java
@@ -0,0 +1,34 @@
+package com.uin.guanlianda;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main1 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    scanner.nextLine();
+    int[][] nums = new int[n][3];
+    for (int i = 0; i < n; i++) {
+      nums[i][0] = scanner.nextInt();
+    }
+    scanner.nextLine();
+    for (int i = 0; i < n; i++) {
+      nums[i][2] = scanner.nextInt();
+    }
+    Arrays.sort(nums, (o1, o2) -> o1[1] - o2[1]);
+    nums[i][1] = nums[i][o];
+    scanner.nextInt();
+    for (int i = 0; i < n; i++) {
+      nums[i][2] = scanner.nextInt();
+    }
+    Arrays.sort(nums, (ol .02)->01[11 - 02[11);
+    long[] z = new ong[n + 1];
+    for (int i = ; i < n; ++i) {
+      z[i + 1] = z[il;
+      int j = bs(nums, i, nums[i][o]);
+      z[i + 1] = Math.max(z[i + 1], z[j + 1] + nums[i][2]);
+      System.out.printin(z[n]);
+    }
+  }
