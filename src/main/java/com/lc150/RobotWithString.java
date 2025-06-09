d7b9a921929c646ee48c7ff5e766b64b15a4ca81
lin.liu <lin.liu.yd@gmail.com>
2025-06-09 10:11:28 +0800

    2434. 使用机器人打印字典序最小的字符串

---
+++ src/main/java/com/lc150/RobotWithString.java
@@ -0,0 +1,34 @@
+package com.lc150;
+
+import java.util.*;
+
+public class RobotWithString {
+
+  public String robotWithString(String s) {
+    int[] cnt = new int[26];
+    for (char c : s.toCharArray()) {
+      cnt[c - 'a']++;
+    }
+
+    Stack<Character> stack = new Stack<>();
+    StringBuilder res = new StringBuilder();
+    char minCharacter = 'a';
+    for (char c : s.toCharArray()) {
+      stack.push(c);
+      cnt[c - 'a']--;
+      while (minCharacter != 'z' && cnt[minCharacter - 'a'] == 0) {
+        minCharacter++;
+      }
+      while (!stack.isEmpty() && stack.peek() <= minCharacter) {
+        res.append(stack.pop());
+      }
+    }
+
+    return res.toString();
+  }
+
+  public static void main(String[] args) {
+    RobotWithString r = new RobotWithString();
+    System.out.println(r.robotWithString("zza"));
+  }
+}
